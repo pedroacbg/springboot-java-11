4866618e37b301adbf71330273730dd395cc1e2f
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2021-09-05 11:09:15 -0300

    Service layer, component registration

---
+++ src/main/java/com/pedroanjos/spring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.pedroanjos.spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedroanjos.spring.entities.User;
+import com.pedroanjos.spring.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
